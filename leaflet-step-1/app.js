3c5c58a5a58ade4bbfd1fb4cc8df87dcb4169e74
BAS <63766016+BonnerDev@users.noreply.github.com>
2020-10-03 21:00:32 +0800

    push changes

--- leaflet-step-1/app.js
+++ leaflet-step-1/app.js
@@ -7,19 +7,13 @@ function createMap(quake) {
   });
 
 // Create the tile layer that will be the background of our map
-  var background = L.tileLayer(
-    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
-    {
-      attribution:
-      "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-      tileSize: 512,
-      maxZoom: 18,
-      zoomOffset: -1,
-      id: "mapbox/streets-v11",
-      accessToken: API_KEY,
-  }
-  ).addTo(myMap);
-​
+  var background = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "light-v10",
+    accessToken: API_KEY
+  });
+
 // Create a baseMaps object to hold the tectonic layer
   var basemaps = {
   "Base Map": background
@@ -36,52 +30,29 @@ function createMap(quake) {
 
 //create a layer control and add to the map
   L.control.layers(background,tectonicmap,quake,{collapsed: false}).addTo(myMap);
-};
-
-
-// function createMarkers(response) {
-//   //pull the quake data
-//   var quakesdata = response.features;
-
-//   //array to hold markers
-//   marker = [];
-
-//   //loop through the features array
-//   for (var index =0; index < features.length, index++){var}
-
-
-
-
-// }
-
-
-
-
-
+}
 // Load in geojson data
-var quakedata =
-  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-​
+var quakedata ="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
 // Grab data with d3
 d3.json(quakedata, (response) => {
-  console.log(station_info);
-  var bikeMarkers = [];
-  var station = station_info.data.stations;
+  console.log(response)});
+  var Markers = [];
+  var depth = response.features.coordinates[2];
   // console.log(station_Info);
-  station.forEach((station) => {
-    var bikeMarker = L.marker([station.lat, station.lon]);
-    var popup = bikeMarker.bindPopup(
-      "<strong>Station Name: </strong>" +
-        station.name +
-        "<br><strong>Station Capacity: </strong><br> " +
-        station.capacity
+  quake.forEach((quake) => {
+    var quakeMarker = L.marker([quake.coordinates[0], quake.coordinates[0]]);
+    var popup = quakeMarker.bindPopup(
+      "<strong>Title: </strong>" +
+        quake.title +
+        "<br><strong>Magnitude: </strong><br> " +
+        quake.mag
     );
-    bikeMarkers.push(bikeMarker);
+    Markers.push(quakeMarker);
   });
-  var bikeLayer = L.layerGroup(bikeMarkers);
-  bikeLayer.addTo(myMap);
+  L.layerGroup(quakeMarkers);
+  quakes.addTo(myMap);
   
   // Perform an API call to the Citi Bike API to get station information. Call createMarkers when complete
-  d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", createMarkers);
+  //d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", createMarkers);
   
